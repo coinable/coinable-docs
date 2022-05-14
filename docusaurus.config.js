b24cefa1c4c04e661b27dd2d173149a551317fab
Denis <dsapaiv@gmail.com>
2022-05-14 22:11:47 +0300

    tweaks

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,10 +89,6 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
                 label: 'GitHub',
                 href: 'https://github.com/coinable',
               },
